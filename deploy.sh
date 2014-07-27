452fe367ebb80f7866faddaa9a43106779b4c01d
Maxim Kot <work.maydjin@gmail.com>
2014-07-27 16:00:54 +0400

    QtCreator solarized scheme was added to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,8 @@ ln -s `pwd`/.screenrc /home/$USER/
 git clone https://github.com/altercation/vim-colors-solarized.git
 mkdir -p ~/.vim/colors
 cp vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
+
+#deploy solarized scheme for QtCreator
+mkdir -p ~/.config/QtProject/qtcreator/styles/
+cp ./qtcreator-solarized/*solar*.xml ~/.config/QtProject/qtcreator/styles/
+
