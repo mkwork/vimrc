6bc3d8e6061908dd27ce3237b7a94e883b20baa6
Maxim Kot <work.maydjin@gmail.com>
2014-09-02 23:30:50 +0400

    Tmux config and deploy was introduced

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,15 @@
 ln -s `pwd`/.vimrc /home/$USER/
 ln -s `pwd`/.gvimrc /home/$USER/
 ln -s `pwd`/.screenrc /home/$USER/
+ln -s `pwd`/.tmux.conf /home/$USER/
 
+#tmux related
+#latest stable tmux
+curl -fsSL https://gist.github.com/shime/5706655/raw/install.sh | sudo bash -e
+#tmux tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+#themes
 #deploy solarized scheme
 git clone https://github.com/altercation/vim-colors-solarized.git
 mkdir -p ~/.vim/colors
@@ -12,4 +20,3 @@ cp vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
 #deploy solarized scheme for QtCreator
 mkdir -p ~/.config/QtProject/qtcreator/styles/
 cp ./qtcreator-solarized/*solar*.xml ~/.config/QtProject/qtcreator/styles/
-
