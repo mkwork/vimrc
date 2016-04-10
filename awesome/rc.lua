443291c02eb4f65f1b6c4c212d2ab94537ce67af
Maxim Kot <work.maydjin@gmail.com>
2016-04-10 16:27:00 +0300

    Awesome default terminal is urxvt now

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt -e tmux"
+terminal = "urxvt"
 editor = "gvim"
 editor_cmd = editor
 
