4ba3752700fdeb83d39c4c53c13d1a42aa401615
Maxim Kot <mkwork@gmail.com>
2016-10-05 23:13:24 +0300

    Solarized themes removed from deploy

--- deploy.sh
+++ deploy.sh
@@ -18,17 +18,4 @@ grep -q "$myrc_init" ~/.bashrc || echo $myrc_init >> ~/.bashrc
 #tmux related
 #make bashr sourcable
 echo 'source ~/.bashrc' >> ~/.bash_profile
-#latest stable tmux
-#curl -fsSL https://gist.github.com/shime/5706655/raw/install.sh | sudo bash -e
-#tmux tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-#themes
-#deploy solarized scheme
-git clone https://github.com/altercation/vim-colors-solarized.git
-mkdir -p ~/.vim/colors
-cp vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
-
-#deploy solarized scheme for QtCreator
-mkdir -p ~/.config/QtProject/qtcreator/styles/
-cp ./qtcreator-solarized/*solar*.xml ~/.config/QtProject/qtcreator/styles/
