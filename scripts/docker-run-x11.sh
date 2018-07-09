a2927364012c911237a7f6f1eccd2954b664dc58
Maxim Kot <work.maydjin@gmail.com>
2018-07-09 22:23:17 +0300

    Docker x11 passing was improved

--- scripts/docker-run-x11.sh
+++ scripts/docker-run-x11.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-
+source docker-pass-x.sh
 docker run  -it \
         --privileged=true \
         --volume=$XSOCK:$XSOCK:rw \
-        --volume=$XAUTH:$XAUTH:rw \
+        --volume=$XAUTHDIR:$XAUTHDIR:rw \
         --volume="/dev/dri:/dev/dri:rw" \
         --env="XAUTHORITY=${XAUTH}" \
         --env="DISPLAY" \
