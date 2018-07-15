548815442257266ffb07d96debe19b93a5cc325c
Maxim Kot <work.maydjin@gmail.com>
2018-07-15 20:34:28 +0300

    add fzf to deploy

--- deploy.sh
+++ deploy.sh
@@ -19,3 +19,6 @@ grep -q "$myrc_init" ~/.bashrc || echo $myrc_init >> ~/.bashrc
 #make bashr sourcable
 echo 'source ~/.bashrc' >> ~/.bash_profile
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
